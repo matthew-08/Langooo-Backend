ac0f9e4ca32fae896dbca8223c405e73093663ac
Matthew <matthewjcrosby1@gmail.com>
2023-03-08 21:32:10 +0700

    message component styling

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -4,29 +4,15 @@ const redisClient = require('../redis').redisClient
 
 
 router.post('/sendMessage', async (req, res) => {
-    const { sender, recipient, date, content, conversationId } = req.body
-
-    // 
-    if(conversationId) {
+    const {timestamp, userId, conversationId, content} = req.body
         // If there's a conversation id that we've already sent to the front end.
         // go ahead and just insert the into the database.
-        const insertMessage = await pool.query('INSERT INTO message (conversation, sender, content, time) VALUES($1, $2, $3, $4)', [conversationId, sender, content, date])
-        return res.json(insertMessage);
-    }
-   
-
-    //we don't have an existing conversation between these two users/
-    // Create a new conversation with the recipient and sender
-    const createNewConversation = await pool.query('INSERT INTO conversation (userId1, userId2) VALUES($1, $2) RETURNING id', [sender, recipient])
-    
-    const returnedConvoId = createNewConversation.rows[0].id
-
-
-    // we created a new conversation, now we'll need to insert thse message into the conversation
-     const createNewMessage = await pool.query('INSERT INTO message (conversation, sender, content, time) VALUES($1, $2, $3, $4)', [returnedConvoId, sender, content, date])
-
-    
-    return res.status(200).json(createNewMessage)
+        const insertMessage = await pool.query(`
+        INSERT INTO message 
+        (conversation, sender, content, time) 
+        VALUES($1, $2, $3, $4)`, 
+        [conversationId, userId, content, timestamp])
+        return res.status(200).json(insertMessage);
 })
 
 router.get('/conversationList/:id', async (req, res) => {
