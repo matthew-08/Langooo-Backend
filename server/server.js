094f83cab55ef05d7c14c1e600ff4f9070a306f4
Matthew <matthewjcrosby1@gmail.com>
2023-03-15 17:56:53 +0700

    fixed cors headers

--- server/server.js
+++ server/server.js
@@ -26,10 +26,7 @@ app.set('trust proxy', 1);
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL,
-    exposedHeaders: ["set-cookie"],
-    headers: {
-        "withCredentials" : true
-        }
+    exposedHeaders: ["set-cookie"]
 }))
 
 io.engine.use(sessionMiddleware);
