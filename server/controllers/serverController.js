9e203e3a515062464eb0bb48affd9a9316b1383d
Matthew <matthewjcrosby1@gmail.com>
2023-06-11 13:09:40 +0700

    [FIX]: Samesite attribute to none on cookie

--- server/controllers/serverController.js
+++ server/controllers/serverController.js
@@ -9,7 +9,7 @@ const sessionMiddleware = session({
     cookie: {
         maxAge: 1000 * 60 * 60,
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         secure: true,
     },
     credentials: true,
